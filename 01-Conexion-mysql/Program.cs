7499f149ceb1145f6017a65ceefcd157c6e3ec42
profeCarballeira <carballeira2024@gmx.es>
2025-12-04 09:21:08 +0100

    query

--- 01-Conexion-mysql/Program.cs
+++ 01-Conexion-mysql/Program.cs
@@ -3,6 +3,7 @@
 class Programa {
     static void Main()
     {
+        //hay que crear un usuario con permisos en la BBDD (localhost o % dependiendo desde que IP se aborde)
         string cadConexion = "server=localhost;port=3307;user=dam2;password=abc123.;database=w3schools;";
         try
         {
---
+++ 02-Query/Program.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+
+class Programa
+{
+    static void Main()
+    {
+        //hay que crear un usuario con permisos en la BBDD (localhost o % dependiendo desde que IP se aborde)
+        string cadConexion = "server=localhost;port=3307;user=dam2;password=abc123.;database=w3schools;";
+        try
+        {
+            //conectar
+            using (MySqlConnection conn = new MySqlConnection(cadConexion))
+            {
+                conn.Open();
+                Console.WriteLine("Conexión con éxito");
+
+                //hacer consulta todos los productos de "Condiments"
+                string query = @"
+                    SELECT p.ProductName, p.Price
+                    FROM products p
+                    JOIN categories c ON p.CategoryID = c.CategoryID
+                    WHERE c.CategoryName = 'Condiments';
+                    ";
+                //crear el comando
+                using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                    using (MySqlDataReader dr = cmd.ExecuteReader()) {
+                        while (dr.Read()) {
+                            Console.WriteLine($"Producto: {dr["ProductName"]}, Precio: {dr["Price"]}");
+                        }
+                    }
+                }
+
+             // manual se cerraría así: conn.Close();
+             //cerrar automático al acabar el using conn
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Excepción: {ex.ToString()}");
+        }
+    }
+
+}
