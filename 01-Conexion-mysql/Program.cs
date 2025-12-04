b9903727af38a5efe0ffc3c164651a93df535c98
luis <luis@A30PC50>
2025-12-04 08:22:09 +0100

    Agregar archivos de proyecto.

---
+++ 01-Conexion-mysql/Program.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+class Programa {
+    static void Main()
+    {
+        string cadConexion = "server=localhost;port=3307;user=dam2;password=abc123.;database=w3schools;";
+        try
+        {
+            //conectar
+            using (MySqlConnection conn = new MySqlConnection(cadConexion))
+            {
+                conn.Open();
+                Console.WriteLine("Conexión con éxito");
+            }
+            //procesar
+
+                //cerrar
+        }
+        catch (Exception ex) { 
+            Console.WriteLine($"Excepción: {ex.ToString()}");
+        }
+    }
+
+}
